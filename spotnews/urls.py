40c4e6bebbaa600b240b9bfb43ae1c3dd2cbf102
leonardo <leonardocaixeta07@gmail.com>
2024-02-26 15:34:25 -0300

    alinhando

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from news.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home, name='home-page'),
+    path("", include("news.urls")),
 ]
 
 if settings.DEBUG:
